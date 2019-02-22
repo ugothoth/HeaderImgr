34ae7e06b58158cb949734a2071936420d181b0a
William Barbosa <heytherewill@gmail.com>
2019-02-22 07:43:41 -0300

    :computer: Add console app

---
+++ HeaderImgr.Cli/CommandLineOptions.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace HeaderImgr.Cli
+{
+    public class CommandLineOptions
+    {
+        [Value(0, HelpText = "Input path of the image")]
+        public string InputPath { get; set; }
+
+        [Value(1, HelpText = "Path to output the image to")]
+        public string OutputPath { get; set; }
+
+        [Option('r', "radius", Required = false, HelpText = "The radius of the blur (defaults to 4)")]
+        public int BlurRadius { get; set; }
+    }
+}
---
+++ HeaderImgr.Cli/Program.cs
@@ -0,0 +1,35 @@
+﻿using CommandLine;
+using HeaderImgr.ImageProcessing.System.Drawing;
+using System;
+using System.Drawing;
+
+namespace HeaderImgr.Cli
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parser.Default
+                .ParseArguments<CommandLineOptions>(args)
+                .WithParsed(onParse);
+        }
+
+        private static void onParse(CommandLineOptions options)
+        {
+            try
+            {
+                var bitmap = new Bitmap(options.InputPath);
+                new NaiveBitmapBoxBlurrer(options.BlurRadius)
+                    .Apply(bitmap)
+                    .Save(options.OutputPath);
+
+                var message = string.Format(Strings.ImageSaveSuccess, options.OutputPath);
+                Console.WriteLine(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
