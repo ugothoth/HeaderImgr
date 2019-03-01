8bddf3d452c199cfa8a6219e67830f0d9d665970
amulware <amul@amulware.net>
2019-03-01 07:44:22 +0000

    ✅ Share tests to cover all box blurring

---
+++ HeaderImgr.Tests/ImageProcessing/System.Drawing/BitmapBoxBlurrerTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using FluentAssertions;
+using FsCheck;
+using FsCheck.Xunit;
+using HeaderImgr.ImageProcessing.System.Drawing;
+using Xunit;
+
+namespace HeaderImgr.Tests.ImageProcessing.System.Drawing
+{
+    public class NaiveBitmapBoxBlurrerTests : BitmapBoxBlurrerBaseTests
+    {
+        protected override IBitmapOperation CreateBlurrerWith(int radius)
+            => new NaiveBitmapBoxBlurrer(radius);
+    }
+
+    public class NaiveCompositeBitmapBoxBlurrerTests : BitmapBoxBlurrerBaseTests
+    {
+        protected override IBitmapOperation CreateBlurrerWith(int radius)
+            => new CompositeBitmapBoxBlurrer(radius);
+    }
+
+    public class NaiveHorizontalVerticalCompositeBoxBlurrerTests : BitmapBoxBlurrerBaseTests
+    {
+        protected override IBitmapOperation CreateBlurrerWith(int radius)
+            => new CompositeBitmapOperation(
+                new HorizontalBitmapBlurrer(radius),
+                new VerticalBitmapBlurrer(radius)
+            );
+    }
+    
+    public class NaiveVerticalHorizontalCompositeBoxBlurrerTests : BitmapBoxBlurrerBaseTests
+    {
+        protected override IBitmapOperation CreateBlurrerWith(int radius)
+            => new CompositeBitmapOperation(
+                new VerticalBitmapBlurrer(radius),
+                new HorizontalBitmapBlurrer(radius)
+            );
+    }
+    
+    public class HorizontalVerticalCompositeBoxBlurrerTests : BitmapBoxBlurrerBaseTests
+    {
+        protected override IBitmapOperation CreateBlurrerWith(int radius)
+            => new CompositeBitmapOperation(
+                new InheritedHorizontalBitmapBlurrer(radius),
+                new InheritedVerticalBitmapBlurrer(radius)
+            );
+    }
+    
+    public class VerticalHorizontalCompositeBoxBlurrerTests : BitmapBoxBlurrerBaseTests
+    {
+        protected override IBitmapOperation CreateBlurrerWith(int radius)
+            => new CompositeBitmapOperation(
+                new InheritedVerticalBitmapBlurrer(radius),
+                new InheritedHorizontalBitmapBlurrer(radius)
+            );
+    }
+}
